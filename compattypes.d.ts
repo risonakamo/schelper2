f1b091d571b996d0ab3ef917777194c79b861692
risonakamo <hmykanae@gmail.com>
2020-06-29 17:33:53 -0400

    initial commit standard setup popup html

---
+++ compattypes.d.ts
@@ -0,0 +1,50 @@
+declare const ReactDOM:any;
+declare const chrome:any;
+declare const moment:any;
+declare const _:any;
+
+declare const Redux:any;
+declare const ReactRedux:any;
+
+declare namespace React
+{
+    class Component
+    {
+        constructor(props:any)
+        setState:Function
+    }
+
+    class PureComponent extends Component
+    {
+
+    }
+
+    const createRef:Function
+}
+
+declare namespace JSX
+{
+    interface IntrinsicElements
+    {
+        [key:string]:any
+    }
+}
+
+type DateString=string;
+
+// chrome tab
+interface Tab
+{
+    url:string
+}
+
+// react's ref
+interface ReactRef<T>
+{
+    current:T
+}
+
+interface ReactProps
+{
+    key?:any
+}
