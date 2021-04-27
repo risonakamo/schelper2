7255808f0404fb207cc4bf735ebba599d29d6740
risonakamo <hmykanae@gmail.com>
2021-04-26 23:59:18 -0400

    SC fit window all works once per tab

--- scripts/fitwindowall.js
+++ scripts/fitwindowall.js
@@ -1,4 +1,12 @@
-// on sankaku image page, execute fit window link
-var iscript=document.createElement("script");
-iscript.src=chrome.runtime.getURL("/scripts/fitwindowall-stage2.js");
-document.head.appendChild(iscript);
+(()=>{
+    // on sankaku image page, execute fit window link
+    if (document.fitToWindow)
+    {
+        return;
+    }
+
+    var iscript=document.createElement("script");
+    iscript.src=chrome.runtime.getURL("/scripts/fitwindowall-stage2.js");
+    document.head.appendChild(iscript);
+    document.fitToWindow=true;
+})();
