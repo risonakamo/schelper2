e37e7a1b1f6654e3062dd0e7fab9a9e3233d0eb4
risonakamo <hmykanae@gmail.com>
2023-03-04 01:50:46 -0500

    fix selector

--- scripts/fanbox-open-big.js
+++ scripts/fanbox-open-big.js
@@ -1,6 +1,6 @@
 // return links to large res on pixiv fanbox
 (()=>{
-    return Array.from(document.querySelectorAll("a.sc-xvj0xk-1")).map((element)=>{
+    return Array.from(document.querySelectorAll("a.sc-xvj0xk-1,a.iWdaRc")).map((element)=>{
         return element.href;
     });
 })()
